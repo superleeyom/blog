01e5574dc9523bde88388d1254927566983af371
superleeyom <635709492@qq.com>
2021-07-01 19:21:12 +0800

    update MD_HEAD

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from github import Github
 MD_HEAD = """**<p align="center">Leeyom's Blog</p>**
 **<p align="center">我的个人博客，用于记录一些幼稚的想法和脑残的瞬间~</p>**
 
-联系方式：
+## 联系方式
 - Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
 - Telegram：[@super_leeyom](https://t.me/super_leeyom)
 - Email：[leeyomwang@163.com](mailto:leeyomwang@163.com)
+- Blog：[https://blog.leeyom.top](https://blog.leeyom.top)
 """
 
 BACKUP_DIR = "backup"
