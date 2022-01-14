926d0ecf9e92e86e9abf1d2075882461dd968598
superleeyom <635709492@qq.com>
2022-01-14 19:27:41 +0800

    修改README的封面

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ MD_HEAD = """**<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
 ====
 
 **<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
-[![](https://user-images.githubusercontent.com/22115219/149496967-05a3d48d-64cc-49c3-81a6-4b9363a2c649.JPEG)](https://blog.leeyom.top)
+[![](https://user-images.githubusercontent.com/22115219/149507985-22e22223-4644-47bf-abf9-c388c287cc38.JPEG)](https://blog.leeyom.top)
 
 ## 联系方式
 - Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
