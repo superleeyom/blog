bbceaf787a30d5e184620c6749cea3734065d05e
superleeyom <635709492@qq.com>
2022-01-14 12:57:38 +0800

    修改README的封面

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 **<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
-**<p align="center">用于记录一些幼稚的想法和脑残的瞬间~</p>**
+====
+
+**<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
+[![](https://user-images.githubusercontent.com/22115219/149452097-7c3656f0-8d6b-49af-a47b-41135aca603b.JPEG)](https://blog.leeyom.top)
 
 ## 联系方式
 - Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import os
 from github import Github
 
 MD_HEAD = """**<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
-**<p align="center">用于记录一些幼稚的想法和脑残的瞬间~</p>**
+====
+
+**<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
+[![](https://user-images.githubusercontent.com/22115219/149452097-7c3656f0-8d6b-49af-a47b-41135aca603b.JPEG)](https://blog.leeyom.top)
 
 ## 联系方式
 - Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
