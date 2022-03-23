d14fe9b3c0ced91f59de62204a1aea933dbf79ea
superleeyom <635709492@qq.com>
2022-03-23 14:06:39 +0800

    暂先不加入友情链接

--- main.py
+++ main.py
@@ -271,7 +271,7 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     repo = get_repo(user, repo_name)
     # add to readme one by one, change order here
     add_md_header("README.md", repo_name)
-    for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
+    for func in [add_md_top, add_md_recent, add_md_label, add_md_todo]:
         func(repo, "README.md", me)
 
     generate_rss_feed(repo, "feed.xml", me)
